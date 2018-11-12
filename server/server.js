8d28d84d35abcdc9b43e2f3c7f7e9e60e04eea96
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2018-11-12 16:49:15 +0530

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -107,6 +107,18 @@ app.get('/users/me', authenticate, (req, res)=>{
     res.send(req.user);
 })
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e)=>{
+        res.status(401).send(e);
+    });
+})
+
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
 });
