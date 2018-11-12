0ded2720139b35f191c8680c2ff3e2fdea005153
Radhakrishnan Meiappan <Radhakrishnan.M@asg.com>
2018-11-12 15:54:22 +0530

    add tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,59 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+    _id: userOneId,
+    email: 'radkkk@yahoo.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'krisrad@gmail.com',
+    password: 'userTwoPass'
+}]
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'first test todo',
+    completed: false
+}, {
+    _id: new ObjectID(),
+    text: 'second test todo',
+    completed: false
+}, {
+    _id: new ObjectID(),
+    text: 'third test todo',
+    completed: true,
+    completedAt : 333
+}];
+
+const populateTodos = (done)=>{    
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(()=>{
+        done();
+    });
+}
+
+const populateUsers = (done) => {
+    User.remove({}).then(()=>{
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+        return Promise.all([userOne, userTwo]);
+    }).then(()=>done());
+}
+
+module.exports = {
+    todos,
+    populateTodos,
+    users,
+    populateUsers
+}
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,29 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'first test todo',
-    completed: false
-}, {
-    _id: new ObjectID(),
-    text: 'second test todo',
-    completed: false
-}, {
-    _id: new ObjectID(),
-    text: 'third test todo',
-    completed: true,
-    completedAt : 333
-}];
-
-beforeEach((done)=>{    
-    Todo.remove({}).then(()=>{
-        return Todo.insertMany(todos);
-    }).then(()=>{
-        done();
-    });
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', ()=> {
     it('should create a new Todo', (done)=>{
@@ -40,14 +22,14 @@ describe('POST /todos', ()=> {
         })
         .end((err, res)=>{
             if (err) {
-                return done();
+                return done(err);
             }
             Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e)=>{
-                done();
+                done(e);
             })
 
         })
@@ -142,7 +124,7 @@ describe('DELETE /todos/:id', ()=>{
                     return done(err);
                 }
                 Todo.findById(hexId).then((todo) => {
-                    expect(todo).toBe(null);
+                    expect(todo).toBeFalsy();
                     done();
                 }).catch((err)=>{
                     done(err);
@@ -176,8 +158,8 @@ describe('PATCH /todos/:id', ()=>{
             .expect(200)
             .expect((res)=>{
                 expect(res.body.todo.text).toBe('repair laptop');                
-                expect(res.body.todo.completed).toBe(true);
-                // expect(res.body.todo.completedAt).toNotBe(null);
+                expect(res.body.todo.completed).toBe(true);                
+                expect(typeof res.body.todo.completedAt).toBe('number');
             })
             .end(done);
     });
@@ -189,9 +171,75 @@ describe('PATCH /todos/:id', ()=>{
             .send({completed:false})
             .expect(200)
             .expect((res)=>{
-                expect(res.body.todo.completedAt).toBe(null);
+                expect(res.body.todo.completedAt).toBeFalsy();
             })
             .end(done);
     });
 
 });
+
+describe('GET /users/me', ()=>{
+    it('should return a user if authenticated', (done)=>{
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+
+    it ('should return a 401 if not authenticated', (done)=>{
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res)=>{
+                expect(res.body).toEqual({});
+            })
+            .end(done);
+    });
+});
+
+describe('POST /users', ()=>{
+    it ('should create a user', (done)=>{
+        var email = 'example@example.com';
+        var password = '123mnb!';
+        request(app)
+            .post('/users')
+            .send({email,password})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.headers['x-auth']).toBeTruthy();
+                expect(res.body._id).toBeTruthy();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err)=>{
+                if (err) {
+                    return done(err);
+                }
+                User.findOne({email}).then((user)=>{
+                    expect(user).toBeTruthy();
+                    expect(user.password).not.toBe(password);
+                    done();
+                });
+            });            
+    });
+
+    it ('should return validation errors if request invalid', (done)=>{
+        request(app)
+            .post('/users')
+            .send({email: 'rk123.com',password: '123mnb!'})
+            .expect(400)
+            .end(done);
+    });
+
+    it ('should not create user if email in use', (done)=>{
+        request(app)
+            .post('/users')
+            .send({email: users[0].email, password: '123mnb!'})
+            .expect(400)
+            .end(done);
+    });
+})
